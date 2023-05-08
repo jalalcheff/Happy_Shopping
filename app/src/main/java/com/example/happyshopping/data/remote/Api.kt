a270f43c1afd2292e556998cc02de6d06c2778ba
jalal cheff <jlalalmyahy571@gmail.com>
2023-05-08 15:07:38 +0300

    create api connection

--- app/src/main/java/com/example/happyshopping/data/remote/Api.kt
+++ app/src/main/java/com/example/happyshopping/data/remote/Api.kt
@@ -1,4 +1,17 @@
 package com.example.happyshopping.data.remote
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
 object Api {
+val BASE_URL="https://fakestoreapi.com"
+    val api = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .build()
+    val apiService = api.create(MarvelService::class.java)
 }
